ddfce19a88397ed5fde3262a4894fcf1b354d539
lz860704 <543972930@qq.com>
2021-03-28 00:09:02 +0800

    update code

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 # -*- coding:utf-8 -*-
 # Author:lz
 
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 
 # 配置参数,开启debug模式,json转换中文不使用unicode
 DEBUG = True
@@ -13,11 +12,31 @@ JSON_AS_ASCII = False
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
 # 配置路由
-@app.route('/dd', methods=['GET'])
-def ping():
-    return jsonify('Flask!')
+@app.route('/lh', methods=['GET'])
+def get_aging():
+    aging = request.args.get('IP_ADDRESS')
+    # print(aging)
+    if (aging == '127.0.0.1'):
+        return dict(
+            IP_ADDRESS='127.0.0.1',
+            AVAILABLE_MEMORY='503',
+            SPACE_SIZE='1823',
+            TCP_CONNECT='130',
+            CPU='6.1',
+            ZOMBIE='0',
+            JAVA_VIRT='3595.8m',
+            JAVA_RES='925.2m',
+            JAVA_CPU='2.5',
+            X1_VIRT='3595.8m',
+            X1_RES='925.2m',
+            X1_CPU='2.5',
+            MYSQL_VIRT='1530.1m',
+            MYSQL_RES='9.6m',
+            MYSQL_CPU='6.3',
+        )
+    else:
+        return '404 Not Found!'
 
 
 if __name__ == "__main__":
